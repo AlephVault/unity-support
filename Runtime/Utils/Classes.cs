083127f694972305243475c32378ddf7e6000941
Luis Masuelli <luisfmasuelli@gmail.com>
2021-04-17 23:30:28 -0500

    Adding Classes and Values utilities

---
+++ Runtime/Utils/Classes.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AlephVault.Unity.Support
+{
+    namespace Utils
+    {
+        /// <summary>
+        ///   This is an extension class with utility methods for types. Many different
+        ///     but class-related utility functions are defined in this class.
+        /// </summary>
+        public static class Classes
+        {
+            /// <summary>
+            ///   Checks whether another type is the same or is a subclass of a base type.
+            /// </summary>
+            /// <param name="derivedType">The derived type to check.</param>
+            /// <param name="baseType">The base type to check against.</param>
+            /// <returns>Whether is the same or subclass, or not.</returns>
+            public static bool IsSameOrSubclassOf(Type derivedType, Type baseType)
+            {
+                return baseType == derivedType || derivedType.IsSubclassOf(baseType);
+            }
+
+            /// <summary>
+            ///   Checks whether another type is the same or is a subclass of a generic type.
+            ///     This is done by unwrapping the generic class on every possible inheritance
+            ///     step in the chain until the top of the hierarchy is reached.
+            /// </summary>
+            /// <param name="genericType">The class to check against - a generic one</param>
+            /// <param name="derivedType">The class to check</param>
+            /// <returns>Whether that class implements, directly or indirectly, the given generic</returns>
+            public static bool IsSubclassOfRawGeneric(Type derivedType, Type genericType)
+            {
+                while (derivedType != null && derivedType != typeof(object))
+                {
+                    var cur = derivedType.IsGenericType ? derivedType.GetGenericTypeDefinition() : derivedType;
+                    if (genericType == cur)
+                    {
+                        return true;
+                    }
+                    derivedType = derivedType.BaseType;
+                }
+                return false;
+            }
+
+            /// <summary>
+            ///   Enumerates all the types that are not generic and are
+            ///     defined in all the currently loaded assemblies in
+            ///     the current application domain.
+            /// </summary>
+            /// <returns>An enumerator of all those types</returns>
+            public static IEnumerable<Type> GetTypes()
+            {
+                return GetTypes(AppDomain.CurrentDomain.GetAssemblies());
+            }
+
+            /// <summary>
+            ///   Enumerates all the types that are not generic and are
+            ///     defined in the given assemblies.
+            /// </summary>
+            /// <returns>An enumerator of all those types</returns>
+            public static IEnumerable<Type> GetTypes(params Assembly[] assemblies)
+            {
+                return from assembly in assemblies
+                       from collectedType in GetTypes(assembly)
+                       select collectedType;
+            }
+
+            /// <summary>
+            ///   Enumerates all the types that are not generic and are
+            ///     defined in the given assembly.
+            /// </summary>
+            /// <returns>An enumerator of all those types</returns>
+            public static IEnumerable<Type> GetTypes(Assembly assembly)
+            {
+                return from assemblyType in assembly.GetTypes()
+                       from collectedType in CollectTypes(assemblyType)
+                       select collectedType;
+            }
+
+            private static IEnumerable<Type> CollectTypes(Type assemblyType)
+            {
+                if (assemblyType.IsGenericType) yield break;
+
+                yield return assemblyType;
+
+                foreach (Type childType in assemblyType.GetNestedTypes())
+                {
+                    foreach (Type collectedType in CollectTypes(childType))
+                    {
+                        yield return collectedType;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/Utils/Values.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+
+namespace AlephVault.Unity.Support
+{
+    namespace Utils
+    {
+        /// <summary>
+        ///   This class acts as a namespace to holds several general-purpose utility methods. Please refer to its methods.
+        /// </summary>
+        public static class Values
+        {
+            /// <summary>
+            ///   A function used as callback to merge dictionary values when invoking the
+            ///     <see cref="Merge{K, V}(Dictionary{K, V}, Dictionary{K, V}, bool, DictionaryMergePicker{K, V})"/>
+            ///     method.
+            /// </summary>
+            /// <typeparam name="K">A dictionary key.</typeparam>
+            /// <typeparam name="V">A dictioanry value.</typeparam>
+            /// <param name="key">The key being merged.</param>
+            /// <param name="leftValue">Value for the key <paramref name="key"/> in the left dictionary.</param>
+            /// <param name="rightValue">Value for the key <paramref name="key"/> in the right dictionary.</param>
+            /// <returns>The final -merged- value.</returns>
+            public delegate V DictionaryMergePicker<K, V>(K key, V leftValue, V rightValue);
+
+            /// <summary>
+            ///   Using a default comparer, returns the minimum value between the two values.
+            /// </summary>
+            /// <typeparam name="T">Type of values to compare. It must be a value type and have a default comparer.</typeparam>
+            /// <param name="a">Value to compare.</param>
+            /// <param name="b">Value to compare.</param>
+            /// <returns>The minimum value.</returns>
+            public static T Min<T>(T a, T b) where T : struct
+            {
+                return (Comparer<T>.Default.Compare(a, b) < 0) ? a : b;
+            }
+
+            /// <summary>
+            ///   Using a default comparer, returns the maximum value between the two values.
+            /// </summary>
+            /// <typeparam name="T">Type of values to compare. It must be a value type and have a default comparer.</typeparam>
+            /// <param name="a">Value to compare.</param>
+            /// <param name="b">Value to compare.</param>
+            /// <returns>The maximum value.</returns>
+            public static T Max<T>(T a, T b) where T : struct
+            {
+                return (Comparer<T>.Default.Compare(a, b) > 0) ? a : b;
+            }
+
+            /// <summary>
+            ///   Using a default comparer, returns a clamped value between the two values, given also a base value.
+            /// </summary>
+            /// <remarks>This means: if the base value is lower than the minimum value, the minimum value is returned. Analogous behaviour goes for the maximum. Otherwise, the base value is returned.</remarks>
+            /// <typeparam name="T">Type of values to compare. It must be a value type and have a default comparer.</typeparam>
+            /// <param name="min">Lower bound.</param>
+            /// <param name="value">Initial value.</param>
+            /// <param name="max">Upper bound.</param>
+            /// <returns>The clamped value.</returns>
+            public static T Clamp<T>(T? min, T value, T? max) where T : struct
+            {
+                if (min == null && max == null)
+                {
+                    return value;
+                }
+                else if (min == null)
+                {
+                    return Min<T>(value, max.Value);
+                }
+                else if (max == null)
+                {
+                    return Max<T>(value, min.Value);
+                }
+                else if (Comparer<T>.Default.Compare(max.Value, min.Value) < 0)
+                {
+                    return Clamp<T>(max, value, min);
+                }
+                return Min<T>(Max<T>(min.Value, value), max.Value);
+            }
+
+            /// <summary>
+            ///   Tells whether a value is clamped between a minimum and a maximum, including them.
+            /// </summary>
+            /// <typeparam name="T">Type of values to compare. It must be a value type and have a default comparer.</typeparam>
+            /// <param name="min">Lower bound.</param>
+            /// <param name="value">Initial value.</param>
+            /// <param name="max">Upper bound.</param>
+            /// <returns>Whether the value is clamped or not.</returns>
+            public static bool In<T>(T? min, T value, T? max) where T : struct
+            {
+                return (min == null || Comparer<T>.Default.Compare(min.Value, value) <= 0) && (max == null || Comparer<T>.Default.Compare(value, max.Value) <= 0);
+            }
+
+            /// <summary>
+            ///   Merges two dictionaries. Merging implies having a dictionary with all the keys from the left and the right dictionaries. If they have the same
+            ///     key, a 
+            /// </summary>
+            /// <typeparam name="K">A dictionary key.</typeparam>
+            /// <typeparam name="V">A dictioanry value.</typeparam>
+            /// <param name="left">The left side dictionary.</param>
+            /// <param name="right">The right side dictionary.</param>
+            /// <param name="inPlace">If <c>true</c> the dictionary being merged is the left. Otherwise, it is a new dictionary having a merge of left and right.</param>
+            /// <param name="picker">The merge function. If absent, a function taking the value from the right side will be used.</param>
+            /// <returns>The merged dictionary. Will be a new one if <paramref name="inPlace"/> is false - otherwise, the left dictionary will be merged and returned</returns>
+            public static Dictionary<K, V> Merge<K, V>(Dictionary<K, V> left, Dictionary<K, V> right, bool inPlace = true, DictionaryMergePicker<K, V> picker = null)
+            {
+                if (picker == null)
+                {
+                    picker = delegate (K key, V leftValue, V rightValue) { return rightValue; };
+                }
+                Dictionary<K, V> destination = inPlace ? left : new Dictionary<K, V>(left);
+                foreach (KeyValuePair<K, V> item in right)
+                {
+                    if (destination.ContainsKey(item.Key))
+                    {
+                        destination[item.Key] = picker(item.Key, destination[item.Key], item.Value);
+                    }
+                    else
+                    {
+                        destination[item.Key] = item.Value;
+                    }
+                }
+                return destination;
+            }
+        }
+    }
+}
